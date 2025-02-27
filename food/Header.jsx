0056e46c5b480cd43c3a4b418caea4abbd4e84c8
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-02-27 21:41:07 +0300

    Add files via upload

---
+++ food/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div style = {{
+        width : "100%" ,
+        backgroundColor : "white" ,
+        height : "180px" ,
+        alignItems : "center" ,
+        justifyContent : "center" ,
+        color : "black"
+    }} >
+    <h1>My Food</h1>
+    </div>
+  )
+}
